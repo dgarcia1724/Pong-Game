c85af568e47c0d2ad60cbdfee317506940f02fe0
Danny Garcia <dgarcia1724@berkeley.edu>
2023-11-02 12:28:42 -0700

    ball class created

--- paddle.py
+++ paddle.py
@@ -1,16 +1,18 @@
-# from turtle import Turtle
-#
-# STARTING_POSITION = (350, 0)
-#
-#
-# class Paddle(Turtle):
-#     def __init__(self):
-#         super().__init__()
-#         self.shape("square")
-#         self.color("white")
-#         self.shapesize(stretch_wid=5, stretch_len=1)
-#         self.penup()
-#         self.goto(STARTING_POSITION[0], STARTING_POSITION[1])
-#
-#     def move_up(self):
-#         self
+from turtle import Turtle
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.shape('square')
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
