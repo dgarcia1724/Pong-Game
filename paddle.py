45f6660a74fd42a12a88e2798bf1e0c84573b7d2
Danny Garcia <dgarcia1724@berkeley.edu>
2023-11-02 11:48:56 -0700

    progress

---
+++ paddle.py
@@ -0,0 +1,16 @@
+# from turtle import Turtle
+#
+# STARTING_POSITION = (350, 0)
+#
+#
+# class Paddle(Turtle):
+#     def __init__(self):
+#         super().__init__()
+#         self.shape("square")
+#         self.color("white")
+#         self.shapesize(stretch_wid=5, stretch_len=1)
+#         self.penup()
+#         self.goto(STARTING_POSITION[0], STARTING_POSITION[1])
+#
+#     def move_up(self):
+#         self
